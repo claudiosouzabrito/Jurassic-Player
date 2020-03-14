b496a7aadd9d4f8c44c80f37672a39aadd605c92
claudiosouzabrito <36930457+claudiosouzabrito@users.noreply.github.com>
2020-03-13 22:54:50 -0300

    Update functions.hpp
    Peguei as funcoes de triangulos e condensei elas

--- Project/source/functions.hpp
+++ Project/source/functions.hpp
@@ -1,6 +1,8 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h> 
 #include <iostream>
+//----------------------------------------------------------------------------------------
+//FUNCOES REFERENTES A JANELAS
 
 //redimenciona a janela para a viewport
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
@@ -15,3 +17,87 @@ void processInput(GLFWwindow *window)
     if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) 
         glfwSetWindowShouldClose(window, true);
 }
+
+void glwindowhint(){
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+}
+//--------------------------------------------------------------------------------------------
+
+//FUNCOES REFERENTES A TRIANGULOS
+
+//fonte dos shaders dos vertices
+const char *vertexShaderSource = "#version 400 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+const char *fragmentShaderSource = "#version 400 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.1f, 0.1f, 1.0f);\n"
+    "}\n\0";
+
+void glVertexsourcecompile(int vertexShader){
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);//carregando fonte ao objeto shader
+
+    glCompileShader(vertexShader);//compilando shader
+
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success); // verificando se houve erro na compilacao
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+}
+
+void glFragmentsourcecompile(int fragmentShader){
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);//carregando fonte ao objeto
+
+    glCompileShader(fragmentShader);// compilando frag shader
+
+    int success;
+    char infoLog[512];
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success); //verificando erro
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+}
+
+void glattachlink(int shaderProgram, int vertexShader, int fragmentShader){
+    glAttachShader(shaderProgram, vertexShader);//aclopando shaders
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    int success;
+    char infoLog[512];
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);// verificando erro
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+    glDeleteShader(vertexShader); //objetos deletados pois nao precisamos mais
+    glDeleteShader(fragmentShader);
+}
+
+void glGenBindvertexarray(unsigned int *VAO){
+    glGenVertexArrays(1, VAO);
+    glBindVertexArray(*VAO);
+}
+/* NAO TO CONSEGUINDO FAZER ESSA FUNCAO FUNCIONAAAAAAAAAAAAAAAAR
+
+void glGenBindvertexbuffer(unsigned int *VBO, float *vertices){
+    glGenBuffers(1, VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, *VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); //carregando vertices no buffer
+
+}
+*/
